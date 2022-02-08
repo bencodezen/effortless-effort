316e30c5c694a22cd328af366b556ccf34f4123e
Ben Hong <ben@bencodezen.io>
2022-02-08 12:56:40 -0500

    feature: add ability to display remaining time in human readable form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <script setup>
 import { ref } from 'vue'
 import { useStorage } from '@vueuse/core'
+import minutesToSeconds from 'date-fns/minutesToSeconds'
+import intervalToDuration from 'date-fns/intervalToDuration'
 
 const newTaskTitle = ref('')
 const taskList = useStorage('task-list', [])
+const intervalId = ref('')
 
 const addTask = () => {
   taskList.value.push({
     title: newTaskTitle.value,
-    estimate: 0
+    estimate: 0,
+    remaining: 0
   })
   newTaskTitle.value = ''
+}
+
+const generateTimeStamp = task => {
+  const remainingTimeLabel = intervalToDuration({
+    start: 0,
+    end: task.remaining * 1000
+  })
+
+  return `${remainingTimeLabel.hours}h ${remainingTimeLabel.minutes}m ${remainingTimeLabel.seconds}s`
+}
+
+const startTimer = task => {
+  if (!task.remaining) {
+    task.remaining = minutesToSeconds(task.estimate)
+  }
+
+  intervalId.value = setInterval(() => {
+    task.remaining--
+  }, 1000)
+}
+
+const stopTimer = () => {
+  clearInterval(intervalId.value)
 }
 </script>
 
@@ -40,12 +67,22 @@ const addTask = () => {
       >
         <article class="task-card">
           <p style="flex: 1; margin: 0">{{ task.title }}</p>
-          <input
-            type="number"
-            style="width: 80px; margin: 0; margin-right: 10px"
-            v-model="task.estimate"
-          />
-          min
+          <div>
+            <div style="margin-bottom: 15px">
+              <input
+                type="number"
+                style="width: 80px; margin: 0; margin-right: 10px"
+                v-model="task.estimate"
+              />
+              min
+            </div>
+            <div style="margin-bottom: 15px">
+              <strong>Remaining Time</strong>:
+              {{ generateTimeStamp(task) }}
+            </div>
+            <button @click="startTimer(task)">Start</button>
+            <button @click="stopTimer" class="secondary">Stop</button>
+          </div>
         </article>
       </li>
     </ul>
